b014e79ed26a70fa115a9f4fb2f015ad1736d160
Samiff <samiff@users.noreply.github.com>
2023-08-01 14:46:52 -0600

    Testing

--- .github/files/setup-wordpress-env.sh
+++ .github/files/setup-wordpress-env.sh
@@ -131,7 +131,8 @@ done
 
 # Install WooCommerce plugin used for some Jetpack integration tests.
 # Todo: check the require paths being used by the Woo tests, they may need updating since the Woo repo structure changed.
-if [[ "$WP_BRANCH" == "latest" && "$PHP_VERSION" == "8.2" ]]; then # Todo: what PHP_VERSION variable is considered "default"?
+echo "TESTING: $PHP_VERSION"
+if [[ "$WP_BRANCH" == "latest" ]]; then # Todo: what PHP_VERSION variable is considered "default"?
 	echo "::group::Installing plugin WooCommerce into WordPress"
 	cd "/tmp/wordpress-$WP_BRANCH/src/wp-content/plugins"
 	git clone --depth=1 https://github.com/woocommerce/woocommerce.git
